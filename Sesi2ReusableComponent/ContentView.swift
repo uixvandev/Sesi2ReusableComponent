7328686ec9e50676abf1e5ba55ec1b11b8c3babc
irfan wahendra <2111527002_irfan@student.unand.ac.id>
2024-01-18 20:12:17 +0700

    Initial Commit

---
+++ Sesi2ReusableComponent/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Sesi2ReusableComponent
+//
+//  Created by irfan wahendra on 18/01/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Sesi2ReusableComponent/Sesi2ReusableComponentApp.swift
@@ -0,0 +1,17 @@
+//
+//  Sesi2ReusableComponentApp.swift
+//  Sesi2ReusableComponent
+//
+//  Created by irfan wahendra on 18/01/24.
+//
+
+import SwiftUI
+
+@main
+struct Sesi2ReusableComponentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
